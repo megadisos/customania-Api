1b4e338ecffdc0f2ef4372fa86405c7f83574ec5
jorge.reales@dialapplet.com <jorge.reales@dialapplet.com>
2023-07-21 22:43:35 -0500

    Feat: Changing enviroment variable

--- src/v1/services/authentication.ts
+++ src/v1/services/authentication.ts
@@ -59,7 +59,7 @@ const loginUser = async (user:User) => {
     const token = req.header('auth-token')
     if (!token) return res.status(401).json({ error: 'Acceso denegado' })
     try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET)
+        const verified = jwt.verify(token, process.env.SERVER_TOKEN)
         req.user = verified
         next() // continuamos
     } catch (error) {
